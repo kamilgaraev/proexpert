acd8e0bbb6edde28afb0bab64b5e99727c5d51d3
kamilgaraev <kamilgaraev11@gmail.com>
2025-05-10 01:27:07 +0300

    321312

--- app/Repositories/OrganizationRepository.php
+++ app/Repositories/OrganizationRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Organization;
 use App\Repositories\Interfaces\OrganizationRepositoryInterface;
+use Illuminate\Support\Collection;
 
 class OrganizationRepository extends BaseRepository implements OrganizationRepositoryInterface
 {
@@ -15,7 +16,32 @@ class OrganizationRepository extends BaseRepository implements OrganizationRepos
         parent::__construct(Organization::class); // Передаем имя класса
     }
 
-    public function findWithUsers(int $id)
+    // Implementations for methods from the old RepositoryInterface
+    public function all(array $columns = ['*']): Collection
+    {
+        return parent::getAll($columns);
+    }
+
+    public function find(int $id, array $columns = ['*']): ?Organization
+    {
+        return parent::findById($id, $columns);
+    }
+
+    public function findBy(string $field, mixed $value, array $columns = ['*']): Collection
+    {
+        return $this->model->where($field, $value)->get($columns);
+    }
+
+    // create(array $data) - предполагаем, что не входит в "4 abstract methods" из-за совпадения имени с parent::create
+    // update(int $id, array $data) - предполагаем, что не входит в "4 abstract methods"
+
+    public function delete(int $id): bool
+    {
+        return parent::deleteById($id);
+    }
+    // End of RepositoryInterface methods
+
+    public function findWithUsers(int $id): ?Organization
     {
         return $this->model->with('users')->find($id);
     }
