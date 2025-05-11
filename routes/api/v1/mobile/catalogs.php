e23e196828b8d91fbf7a4a8aa6202801a46a2678
kamilgaraev <kamilgaraev11@gmail.com>
2025-05-11 21:06:04 +0300

    312

--- routes/api/v1/mobile/catalogs.php
+++ routes/api/v1/mobile/catalogs.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Api\V1\Mobile\MaterialController;
 use App\Http\Controllers\Api\V1\Mobile\SupplierController;
 // Добавьте сюда другие контроллеры для мобильных справочников по мере необходимости
 
-Route::middleware(['auth:api_mobile'/*, 'can:access-mobile-app'*/])->group(function () {
+Route::middleware(['auth:api_mobile', 'can:access-mobile-app'])->group(function () {
     Route::get('work-types', [WorkTypeController::class, 'index'])->name('mobile.work-types.index');
     Route::get('materials', [MaterialController::class, 'index'])->name('mobile.materials.index');
     Route::get('suppliers', [SupplierController::class, 'index'])->name('mobile.suppliers.index');
