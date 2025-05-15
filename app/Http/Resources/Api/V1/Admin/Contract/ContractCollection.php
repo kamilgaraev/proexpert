4edddfbdacf77393b94c43ef7680923739d6959e
kamilgaraev <kamilgaraev11@gmail.com>
2025-05-16 00:49:01 +0300

    321

--- app/Http/Resources/Api/V1/Admin/Contract/ContractCollection.php
+++ app/Http/Resources/Api/V1/Admin/Contract/ContractCollection.php
@@ -1,1 +1,42 @@
- 
+<?php
+
+namespace App\Http\Resources\Api\V1\Admin\Contract;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ContractCollection extends ResourceCollection
+{
+    /**
+     * The resource that this resource collects.
+     *
+     * @var string
+     */
+    public $collects = ContractResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'data' => $this->collection,
+            // Вы можете добавить метаданные пагинации здесь, если они доступны
+            // 'links' => [
+            //     'self' => 'link-values',
+            // ],
+            // 'meta' => [
+            //     // 'current_page' => $this->currentPage(),
+            //     // 'from' => $this->firstItem(),
+            //     // 'last_page' => $this->lastPage(),
+            //     // 'path' => $this->path(),
+            //     // 'per_page' => $this->perPage(),
+            //     // 'to' => $this->lastItem(),
+            //     // 'total' => $this->total(),
+            // ],
+        ];
+    }
+} 
