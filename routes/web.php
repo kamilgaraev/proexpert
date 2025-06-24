c72cd6fbfff25fe5fc5962b29725888669486bb3
kamilgaraev <kamilgaraev11@gmail.com>
2025-06-25 00:58:45 +0300

    Добавлен новый маршрут /simple-test для простого тестирования без контроллера, возвращающий текстовые метрики. Это улучшает возможности тестирования и упрощает отладку системы.

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,12 @@ Route::get('/login', function () {
     return response()->json(['message' => 'Please use API endpoints for authentication'], 401);
 })->name('login');
 
+// Простой тест без контроллера
+Route::get('/simple-test', function () {
+    return response("# Simple test\ntest_metric 1\n", 200, [
+        'Content-Type' => 'text/plain; charset=utf-8'
+    ]);
+});
+
 Route::get('/metrics', [App\Http\Controllers\MetricsController::class, 'metrics']);
 Route::get('/test-metrics', [App\Http\Controllers\MetricsController::class, 'test']);
