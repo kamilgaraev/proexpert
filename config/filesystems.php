1ccef3e5c1757b41f4261820e75cc7d4aa7ac8bc
kamilgaraev <kamilgaraev11@gmail.com>
2025-09-12 23:57:34 +0300

    feat(storage): обновление конфигурации бакета для хранения файлов
    - Изменен основной бакет в конфигурации файловой системы на 'prohelper-storage', что обеспечивает централизованное хранение для всех организаций.
    - Эти изменения способствуют улучшению управления хранилищем и упрощают тестирование.

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,7 +53,7 @@ return [
             'key'    => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION', 'ru-central1'),
-            'bucket' => env('AWS_DUMMY_BUCKET', 'org-1-qo7jh2'), // используем существующий бакет для тестирования
+            'bucket' => 'prohelper-storage', // основной бакет для всех организаций
             // bucket устанавливается динамически в OrgBucketService
             'endpoint' => env('AWS_ENDPOINT', 'https://storage.yandexcloud.net'),
             'use_path_style_endpoint' => false, // virtual-hosted стиль
