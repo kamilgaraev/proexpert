4dd0268b7297f256b55845d384ce22ca5f4c16a7
kamilgaraev <kamilgaraev11@gmail.com>
2025-10-10 14:53:16 +0300

    feat(ContractWorkTypeCategory): добавление новых категорий работ по контракту
    - Расширен enum ContractWorkTypeCategoryEnum, добавлены новые категории: GENERAL_CONSTRUCTION, FINISHING, INSTALLATION, DESIGN, CONSULTATION.
    - Обновлены схемы OpenAPI для создания, обновления и получения контрактов, чтобы включить новые категории работ.
    - Эти изменения улучшают функциональность API, позволяя более точно классифицировать виды работ по контрактам.

--- app/Enums/Contract/ContractWorkTypeCategoryEnum.php
+++ app/Enums/Contract/ContractWorkTypeCategoryEnum.php
@@ -5,6 +5,11 @@ namespace App\Enums\Contract;
 enum ContractWorkTypeCategoryEnum: string
 {
     case SMR = 'smr'; // Строительно-монтажные работы
+    case GENERAL_CONSTRUCTION = 'general_construction'; // Общестроительные работы
+    case FINISHING = 'finishing'; // Отделочные работы
+    case INSTALLATION = 'installation'; // Монтажные работы
+    case DESIGN = 'design'; // Проектирование
+    case CONSULTATION = 'consultation'; // Консультационные услуги
     case SUPPLY = 'supply'; // Поставка
     case SERVICES = 'services'; // Услуги
     case RENT = 'rent'; // Аренда
