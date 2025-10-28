b66515beaa9b90f193959e42b40f409f0000f455
kamilgaraev <kamilgaraev11@gmail.com>
2025-10-28 11:23:44 +0300

    refactor(NotificationBroadcast): change broadcasting interface from ShouldBroadcastNow to ShouldBroadcast
    - Updated the NotificationBroadcast class to implement ShouldBroadcast instead of ShouldBroadcastNow, aligning with the latest broadcasting standards in Laravel.
    - This change enhances the flexibility of the broadcasting implementation, allowing for better integration with the broadcasting system.

--- app/BusinessModules/Features/Notifications/Events/NotificationBroadcast.php
+++ app/BusinessModules/Features/Notifications/Events/NotificationBroadcast.php
@@ -6,11 +6,11 @@ use App\BusinessModules\Features\Notifications\Models\Notification;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationBroadcast implements ShouldBroadcastNow
+class NotificationBroadcast implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
