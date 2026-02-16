7686eb1c3bac04592344aa94125223fcc3b9c293
kamilgaraev <kamilgaraev11@gmail.com>
2026-02-16 13:10:51 +0300

    фикс

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -20,6 +20,21 @@ class UserResource extends Resource
 
     protected static string | \UnitEnum | null $navigationGroup = 'System';
 
+    public static function getNavigationLabel(): string
+    {
+        return __('widgets.users.navigation_label');
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('widgets.users.model_label');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('widgets.users.plural_model_label');
+    }
+
     public static function form(Schema $schema): Schema
     {
         return $schema
