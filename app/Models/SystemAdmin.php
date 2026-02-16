e1a602d63ba524f874e2bd0cbe9b2678bc5f188c
kamilgaraev <kamilgaraev11@gmail.com>
2026-02-16 10:48:45 +0300

    fix

--- app/Models/SystemAdmin.php
+++ app/Models/SystemAdmin.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Filament\Models\Contracts\FilamentUser;
 
-class SystemAdmin extends Authenticatable
+class SystemAdmin extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -40,4 +41,9 @@ class SystemAdmin extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return $panel->getId() === 'admin';
+    }
 }
