87a6a9e48375eae0214ad7d08cc638122a9cfb6d
kamilgaraev <kamilgaraev11@gmail.com>
2026-02-16 13:05:53 +0300

    2ё1

---
+++ resources/lang/ru/widgets.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'saas_income_stats' => [
+        'total_income' => 'Общий доход SaaS',
+        'monthly_income' => 'Доход за месяц',
+        'all_time_description' => 'Все успешные платежи за все время',
+        'monthly_description' => 'Доход за :date',
+    ],
+    'orders_stats' => [
+        'total_orders' => 'Всего заказов',
+        'pending_orders' => 'Ожидают обработки',
+        'completed_orders' => 'Завершенные заказы',
+        'new_orders_description' => 'Новых заказов за этот месяц',
+        'pending_description' => 'Требуют внимания менеджера',
+        'completed_description' => 'Успешно выполненные заказы',
+    ],
+    'users_stats' => [
+        'total_users' => 'Всего пользователей',
+        'new_users' => 'Новые пользователи',
+        'total_description' => 'Зарегистрированных пользователей',
+        'new_description' => 'Присоединились за этот месяц',
+    ],
+    'subscription_plans' => [
+        'navigation_label' => 'Тарифы',
+        'model_label' => 'Тариф',
+        'plural_model_label' => 'Тарифные планы',
+        'section_main' => 'Основная информация',
+        'section_features' => 'Возможности тарифа',
+        'name' => 'Название',
+        'slug' => 'Slug (API ID)',
+        'price' => 'Цена',
+        'period' => 'Период оплаты',
+        'is_active' => 'Активен',
+        'is_default' => 'По умолчанию',
+        'features' => 'Список возможностей',
+        'feature_name' => 'Название функции',
+        'feature_value' => 'Значение (опционально)',
+    ],
+];
