a60e6e84c11d965e93f8e58b42a26b3ba8734acd
kamilgaraev <kamilgaraev11@gmail.com>
2026-02-16 08:56:03 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -118,8 +118,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->register(\App\BusinessModules\Core\Organizations\OrganizationsServiceProvider::class);
         $this->app->register(\App\BusinessModules\Core\Users\UsersServiceProvider::class);
         $this->app->register(\App\BusinessModules\Core\MultiOrganization\MultiOrganizationServiceProvider::class);
-        $this->app->register(\App\BusinessModules\Features\BasicReports\BasicReportsServiceProvider::class);
-        $this->app->register(\App\BusinessModules\Features\AdvancedReports\AdvancedReportsServiceProvider::class);
+        $this->app->register(\App\BusinessModules\Features\Reports\ReportsServiceProvider::class);
         $this->app->register(\App\BusinessModules\Features\AdvancedDashboard\AdvancedDashboardServiceProvider::class);
         $this->app->register(\App\BusinessModules\Enterprise\MultiOrganization\Reporting\ReportingServiceProvider::class);
         $this->app->register(\App\BusinessModules\Enterprise\MultiOrganization\Core\MultiOrganizationEventServiceProvider::class);
