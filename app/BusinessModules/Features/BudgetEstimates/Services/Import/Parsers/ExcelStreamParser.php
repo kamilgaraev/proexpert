99b1a5537a77553eddb85fa70324b2e914b1199b
kamilgaraev <kamilgaraev11@gmail.com>
2026-02-16 20:08:20 +0300

    312

--- app/BusinessModules/Features/BudgetEstimates/Services/Import/Parsers/ExcelStreamParser.php
+++ app/BusinessModules/Features/BudgetEstimates/Services/Import/Parsers/ExcelStreamParser.php
@@ -27,9 +27,18 @@ class ExcelStreamParser implements StreamParserInterface
             // readRows() is a generator method in recent versions of SimpleXLSX
             // If strictly using rows(), it might load all. 
             // Checking library capability: standard SimpleXLSX usually has readRows($sheetIndex = 0)
+            $rowCount = 0;
             foreach ($xlsx->readRows() as $row) {
+                if ($rowCount < 5) {
+                    \Illuminate\Support\Facades\Log::debug('[ExcelStreamParser] Row data', [
+                        'row_num' => $rowCount,
+                        'data' => array_slice($row, 0, 5)
+                    ]);
+                }
+                $rowCount++;
                 yield $row;
             }
+            \Illuminate\Support\Facades\Log::info('[ExcelStreamParser] Finished reading', ['total_rows' => $rowCount]);
         } else {
             throw new RuntimeException("Failed to parse file: " . SimpleXLSX::parseError());
         }
