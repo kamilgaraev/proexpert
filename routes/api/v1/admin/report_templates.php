c50517a4c0adb4b8954c00ea1b5e177223f73f4c
kamilgaraev <kamilgaraev11@gmail.com>
2026-03-31 17:11:03 +0300

    fix: открыт просмотр шаблонов отчетов для пользователей отчетов

--- routes/api/v1/admin/report_templates.php
+++ routes/api/v1/admin/report_templates.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\Api\V1\Admin\ReportTemplateController;
 
 Route::prefix('report-templates')->name('report-templates.')->group(function () {
     Route::get('/', [ReportTemplateController::class, 'index'])
-        ->middleware('authorize:report_templates.view')
+        ->middleware('authorize:reports.view')
         ->name('index');
     Route::post('/', [ReportTemplateController::class, 'store'])
         ->middleware('authorize:report_templates.create')
         ->name('store');
     Route::get('/{templateId}', [ReportTemplateController::class, 'show'])
-        ->middleware('authorize:report_templates.view')
+        ->middleware('authorize:reports.view')
         ->name('show');
     Route::match(['put', 'patch'], '/{templateId}', [ReportTemplateController::class, 'update'])
         ->middleware('authorize:report_templates.edit')
