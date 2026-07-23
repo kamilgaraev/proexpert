d5143a2269ac6141dba9b6b2669b16a86650959d
kamilgaraev <kamilgaraev11@gmail.com>
2026-07-23 21:14:55 +0300

    fix[lk]: увеличен лимит загрузки в api контейнере

--- tests/Unit/EstimateGeneration/EstimateGenerationUploadLimitConfigurationTest.php
+++ tests/Unit/EstimateGeneration/EstimateGenerationUploadLimitConfigurationTest.php
@@ -34,4 +34,13 @@ class EstimateGenerationUploadLimitConfigurationTest extends TestCase
             }
         }
     }
+
+    public function test_api_container_php_runtime_allows_two_hundred_megabyte_uploads(): void
+    {
+        $contents = file_get_contents(dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . 'docker-compose.yml');
+
+        self::assertStringContainsString('-d post_max_size=640M', (string) $contents);
+        self::assertStringContainsString('-d upload_max_filesize=500M', (string) $contents);
+        self::assertStringContainsString('-d max_file_uploads=100', (string) $contents);
+    }
 }
